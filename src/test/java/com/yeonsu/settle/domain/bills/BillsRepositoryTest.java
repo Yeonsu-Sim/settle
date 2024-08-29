e3b5eb232c81ed4ae0e8f28b465148d2e6d27728
yeonsu <shenlianxiu@gmail.com>
2024-08-29 19:20:12 +0900

    test: JPA Auditing - Bills Entity

--- src/test/java/com/yeonsu/settle/domain/bills/BillsRepositoryTest.java
+++ src/test/java/com/yeonsu/settle/domain/bills/BillsRepositoryTest.java
@@ -55,4 +55,41 @@ public class BillsRepositoryTest {
         assertThat(bills.getProduct()).isEqualTo(product);
         assertThat(bills.getPayer()).isEqualTo(payer);
     }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2024, 8, 29, 0, 0, 0);
+
+        String product = "테스트 영수증";
+        Long amount = 10000L;
+        String payer = "연수";
+        LocalDateTime dateTime = LocalDateTime.now();
+
+        List<String> participants = new ArrayList<String>();
+        participants.add("연수");
+        participants.add("연화");
+        participants.add("윤성");
+
+        billsRepository.save(Bills.builder()
+                .product(product)
+                .amount(amount)
+                .payer(payer)
+                .participants(participants)
+                .dateTime(dateTime)
+                .memo(null)
+                .build());
+
+        // when
+        List<Bills> billsList = billsRepository.findAll();
+
+        // then
+        Bills bills = billsList.get(0);
+
+        System.out.println(">>>>>>>>> createdDate=" + bills.getCreatedDate() +
+                ", modifiedDate=" + bills.getModifiedDate());
+
+        assertThat(bills.getCreatedDate()).isAfter(now);
+        assertThat(bills.getModifiedDate()).isAfter(now);
+    }
 }
